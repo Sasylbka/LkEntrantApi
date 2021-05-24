b67a8c4dcaf4d6b1656f5f859f1c1f9e30fda565
druzhinin <saske12321@gmail.com>
2021-05-24 17:28:41 +0800

    Вывод заявок при отствутвии данных в таблицах

--- src/main/java/ru/esstu/entrant/lk/repositories/QuestionnaireRepository.java
+++ src/main/java/ru/esstu/entrant/lk/repositories/QuestionnaireRepository.java
@@ -7,19 +7,23 @@ import ru.esstu.entrant.lk.domain.vo.Questionnaire;
 import java.util.List;
 @Mapper
 public interface QuestionnaireRepository {
-    @Select("SELECT " +
-            "entrant_private_data.id, entrant_private_data.entrant_id, " +
-            "entrant_private_data.name," +
-            "entrant_private_data.family_name," +
-            "entrant_private_data.patronymic, admission_info.level_of_education," +
-            "changes_date.date_of_creation,changes_date.date_of_send,changes_date.date_of_answer," +
-            "admission_info.contract, entrant.status " +
-            "FROM " +
-            "entrant_private_data, entrant, admission_info, changes_date " +
-            "WHERE " +
-            "entrant.id=entrant_private_data.entrant_id " +
-            "and entrant.id=admission_info.entrant_id " +
-            "and entrant.id=changes_date.entrant_id ")
+    @Select("SELECT "+
+            "entrant.id,"+
+            "entrant_private_data.id,"+
+            "entrant_private_data.name,"+
+            "entrant_private_data.family_name,"+
+            "entrant_private_data.patronymic,"+
+            "admission_info.level_of_education,"+
+            "changes_date.date_of_creation,"+
+            "changes_date.date_of_send,"+
+            "changes_date.date_of_answer,"+
+            "admission_info.contract,"+
+            "entrant.status "+
+            "FROM "+
+            "entrant "+
+            "Left join entrant_private_data on entrant.id=entrant_private_data.entrant_id "+
+            "left join admission_info on entrant.id=admission_info.entrant_id "+
+            "left join changes_date on entrant.id=changes_date.entrant_id ")
     List<Questionnaire> getQuestionnaire();
 
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
