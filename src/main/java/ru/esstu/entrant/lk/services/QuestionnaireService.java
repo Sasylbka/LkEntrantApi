c28c2ef0e50d5013c4ca55dc0a3bd8ca6e3fbd7c
Nikita Veryasov <niktinw@gmail.com>
2021-06-28 20:09:46 +0800

    Фикс QuestionnaireService 2

--- src/main/java/ru/esstu/entrant/lk/services/QuestionnaireService.java
+++ src/main/java/ru/esstu/entrant/lk/services/QuestionnaireService.java
@@ -40,10 +40,10 @@ public class QuestionnaireService {
     public Boolean checkAccel(QuestionnaireDto questionnaireDto) {//Проверка на ускоренное обучение
         List<AdmissionInfo> aiList = admissionInfoRepository.getAdmissionInfos(questionnaireDto.getId());
         if (aiList.size() == 0) {
-            return null;
+            return false;
         }
-        else for (int i = 0; i < aiList.size(); i++) {
-            if (specialityRefRepository.getFacultyId(Long.parseLong(aiList.get(i).getDirection()))==13) {
+        for (int i = 0; i < aiList.size(); i++) {
+            if (specialityRefRepository.getFacultyId(Long.parseLong(aiList.get(i).getDirection())) == 13) {
                 return true;
             }
         }
@@ -60,8 +60,8 @@ public class QuestionnaireService {
         for (int i = 1; i < temp.size(); i++) {
             if (!list.contains(temp.get(i).getId())) {
                 list.add(temp.get(i).getId());
-
                 temp1.add(temp.get(i));
+                temp1.get(i).setAccelerated(false);
             }
         }
         for (int i = 0; i < temp1.size(); i++) {
