6fb3dd76049336dd60031de4b667793890c9e9ce
druzhinin <saske12321@gmail.com>
2021-07-23 19:26:08 +0800

    Test №7 и ещё одна кодировочка

--- src/main/java/ru/esstu/entrant/lk/services/forpdf/DocxGeneratorService.java
+++ src/main/java/ru/esstu/entrant/lk/services/forpdf/DocxGeneratorService.java
@@ -294,7 +294,7 @@ public class DocxGeneratorService {
         //InputStream docxFile = new FileInputStream(Objects.requireNonNull(this.getClass().getResourceAsStream(TEMP_NAME));
         Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.XWPF);
         PdfOptions pdfOptions = PdfOptions.create();
-        pdfOptions.fontEncoding("UTF-8");
+        pdfOptions.fontEncoding("ANSI");
         options.subOptions(pdfOptions);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         report.convert(context,options,outputStream);
