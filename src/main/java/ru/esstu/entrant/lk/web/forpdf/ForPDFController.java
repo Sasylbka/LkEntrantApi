5ae0ca652c585e16f26a3d43a756f1a2ec803194
druzhinin <saske12321@gmail.com>
2021-07-24 17:41:58 +0800

    Test №18

--- src/main/java/ru/esstu/entrant/lk/web/forpdf/ForPDFController.java
+++ src/main/java/ru/esstu/entrant/lk/web/forpdf/ForPDFController.java
@@ -3,34 +3,34 @@ package ru.esstu.entrant.lk.web.forpdf;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.esstu.entrant.lk.domain.dto.FileDto;
-import ru.esstu.entrant.lk.services.FileService;
 import ru.esstu.entrant.lk.services.forpdf.DocxGeneratorService;
+import ru.esstu.entrant.lk.services.forpdf.DocxGeneratorServiceTest;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
-import java.util.List;
 
 @RestController
 @Slf4j
 
 public class ForPDFController {
+    private final DocxGeneratorServiceTest docxGeneratorServiceTest;
     private final DocxGeneratorService docxGeneratorService;
 
-    public ForPDFController( DocxGeneratorService docxGeneratorService) {
+    public ForPDFController(DocxGeneratorService docxGeneratorService, DocxGeneratorServiceTest docxGeneratorServiceTest) {
+        this.docxGeneratorServiceTest = docxGeneratorServiceTest;
         this.docxGeneratorService = docxGeneratorService;
     }
-    @RequestMapping(method = RequestMethod.GET, path = "/getDoc")
+    @RequestMapping(method = RequestMethod.GET, path = "/getDoc.pdf")
     public void get(final int id, HttpServletResponse response) throws Exception {
+        byte [] result = docxGeneratorService.generateDocxFileFromTemplate(id);
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "filename=\"file.pdf\"");
+        response.getOutputStream().write(result);
+    }
+    @RequestMapping(method = RequestMethod.GET, path = "/getDoc")
+    public void getDocx(final int id, HttpServletResponse response) throws Exception {
         byte[] result;
-        result = docxGeneratorService.generateDocxFileFromTemplate(id);
+        result = docxGeneratorServiceTest.generateDocxFileFromTemplate(id);
         response.setHeader("Content-Disposition", "attachment; filename=\"message.docx\"");
         response.getOutputStream().write(result);
     }
